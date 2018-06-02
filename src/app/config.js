0560a711f5de9445f9ddef4b906fe94232383793
jinsphan <pvtinh1996@gmail.com>
2018-06-02 23:38:57 +0700

    Update

--- src/app/config.js
+++ src/app/config.js
@@ -1,10 +1,10 @@
 let config = require("../config.development.json");
 
-if (process.env.REACT_APP_NODE_ENV === "staging") {
+if (process.env.NODE_ENV === "staging") {
   config = require("../config.staging.json");
 }
 
-if (process.env.REACT_APP_NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   config = require("../config.production.json");
 }
 
